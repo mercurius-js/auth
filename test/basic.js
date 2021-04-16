3b0644185a9d84f4772d02ab2f4f9cc8390d45a2
Jonny Green <hello@jonnydgreen.com>
2021-04-16 12:16:44 +0100

    Add support for nested fields

--- test/basic.js
+++ test/basic.js
@@ -17,17 +17,19 @@ const schema = `
     UNKNOWN
   }
 
+  type Message {
+    title: String!
+    public: String!
+    private: String! @auth(requires: ADMIN)
+  }
+
   type Query {
     add(x: Int, y: Int): Int @auth(requires: ADMIN)
     subtract(x: Int, y: Int): Int
+    messages: [Message!]!
+    adminMessages: [Message!]! @auth(requires: ADMIN)
   }
 `
-
-const query = `query {
-  four: add(x: 2, y: 2)
-  six: add(x: 3, y: 3)
-  subtract(x: 3, y: 3)
-}`
 
 const resolvers = {
   Query: {
@@ -38,11 +40,39 @@ const resolvers = {
     subtract: async (_, obj) => {
       const { x, y } = obj
       return x - y
+    },
+    messages: async () => {
+      return [
+        {
+          title: 'one',
+          public: 'public one',
+          private: 'private one'
+        },
+        {
+          title: 'two',
+          public: 'public two',
+          private: 'private two'
+        }
+      ]
+    },
+    adminMessages: async () => {
+      return [
+        {
+          title: 'admin one',
+          public: 'admin public one',
+          private: 'admin private one'
+        },
+        {
+          title: 'admin two',
+          public: 'admin public two',
+          private: 'admin private two'
+        }
+      ]
     }
   }
 }
 
-test('registration - should protect the schema not affect queries when everything is okay', async (t) => {
+test('registration - should protect the schema and not affect queries when everything is okay', async (t) => {
   t.plan(1)
 
   const app = Fastify()
@@ -63,6 +93,22 @@ test('registration - should protect the schema not affect queries when everythin
     }
   })
 
+  const query = `query {
+  four: add(x: 2, y: 2)
+  six: add(x: 3, y: 3)
+  subtract(x: 3, y: 3)
+  messages {
+    title
+    public
+    private
+  }
+  adminMessages {
+    title
+    public
+    private
+  }
+}`
+
   const response = await app.inject({
     method: 'POST',
     headers: { 'content-type': 'application/json', 'X-User': 'admin' },
@@ -74,7 +120,31 @@ test('registration - should protect the schema not affect queries when everythin
     data: {
       four: 4,
       six: 6,
-      subtract: 0
+      subtract: 0,
+      messages: [
+        {
+          title: 'one',
+          public: 'public one',
+          private: 'private one'
+        },
+        {
+          title: 'two',
+          public: 'public two',
+          private: 'private two'
+        }
+      ],
+      adminMessages: [
+        {
+          title: 'admin one',
+          public: 'admin public one',
+          private: 'admin private one'
+        },
+        {
+          title: 'admin two',
+          public: 'admin public two',
+          private: 'admin private two'
+        }
+      ]
     }
   })
 })
@@ -100,6 +170,22 @@ test('registration - should protect the schema and error accordingly', async (t)
     }
   })
 
+  const query = `query {
+  four: add(x: 2, y: 2)
+  six: add(x: 3, y: 3)
+  subtract(x: 3, y: 3)
+  messages {
+    title
+    public
+    private
+  }
+  adminMessages {
+    title
+    public
+    private
+  }
+}`
+
   const response = await app.inject({
     method: 'POST',
     headers: { 'content-type': 'application/json', 'X-User': 'user' },
@@ -111,32 +197,41 @@ test('registration - should protect the schema and error accordingly', async (t)
     data: {
       four: null,
       six: null,
-      subtract: 0
+      subtract: 0,
+      messages: [
+        {
+          title: 'one',
+          public: 'public one',
+          private: null
+        },
+        {
+          title: 'two',
+          public: 'public two',
+          private: null
+        }
+      ],
+      adminMessages: null
     },
     errors: [
       {
         message: 'auth error',
-        locations: [
-          {
-            line: 2,
-            column: 3
-          }
-        ],
-        path: [
-          'four'
-        ]
+        locations: [{ line: 2, column: 3 }],
+        path: ['four']
       },
       {
         message: 'auth error',
-        locations: [
-          {
-            line: 3,
-            column: 3
-          }
-        ],
-        path: [
-          'six'
-        ]
+        locations: [{ line: 3, column: 3 }],
+        path: ['six']
+      },
+      {
+        message: 'auth error',
+        locations: [{ line: 10, column: 3 }],
+        path: ['adminMessages']
+      },
+      {
+        message: 'auth error',
+        locations: [{ line: 8, column: 5 }],
+        path: ['messages', 'private']
       }
     ]
   })
@@ -164,6 +259,24 @@ test('registration - should work alongside existing directives', async (t) => {
       subtract(x: Int, y: Int): Int @notUsed
     }
 `
+  const resolvers = {
+    Query: {
+      add: async (_, obj) => {
+        const { x, y } = obj
+        return x + y
+      },
+      subtract: async (_, obj) => {
+        const { x, y } = obj
+        return x - y
+      }
+    }
+  }
+
+  const query = `query {
+  four: add(x: 2, y: 2)
+  six: add(x: 3, y: 3)
+  subtract(x: 3, y: 3)
+}`
 
   const app = Fastify()
   t.teardown(app.close.bind(app))
