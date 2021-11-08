0c9d9ae184681ede8d3f7745a406687efba81b3d
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-11-08 16:02:04 +0100

    fix multiple authcontext directive

--- test/advanced.js
+++ test/advanced.js
@@ -174,7 +174,7 @@ test('should be able to access the query to determine that users have insufficie
 })
 
 test('should support being registered multiple times', async (t) => {
-  t.plan(2)
+  t.plan(8)
 
   const app = Fastify()
   t.teardown(app.close.bind(app))
@@ -203,6 +203,14 @@ test('should support being registered multiple times', async (t) => {
     }
   }
 
+  const userType = [{
+    identity: 'user',
+    foo: 'bar'
+  }, {
+    identity: 'super-user',
+    foo: 'bar'
+  }]
+
   app.register(mercurius, {
     schema,
     resolvers
@@ -210,17 +218,25 @@ test('should support being registered multiple times', async (t) => {
 
   app.register(mercuriusAuth, {
     authContext (context) {
+      t.notOk(context.auth, 'this is the first hook executed')
       return {
         identity: context.reply.request.headers['x-user']
       }
     },
     async applyPolicy (authDirectiveAST, parent, args, context, info) {
+      t.same(context.auth, userType.shift())
       return context.auth.identity.includes('user')
     },
     authDirective: 'auth1'
   })
 
   app.register(mercuriusAuth, {
+    authContext (context) {
+      t.ok(context.auth.identity, 'this is the second hook executed')
+      return {
+        foo: 'bar'
+      }
+    },
     async applyPolicy (authDirectiveAST, parent, args, context, info) {
       return context.auth.identity === 'super-user'
     },
@@ -274,6 +290,155 @@ test('should support being registered multiple times', async (t) => {
       data: {
         add: 3,
         subtract: 1
+      }
+    })
+  }
+})
+
+test('register multiple times readme example', async (t) => {
+  t.plan(4)
+
+  const app = Fastify()
+  t.teardown(app.close.bind(app))
+
+  const schema = `
+  directive @hasRole(
+    type: String,
+  ) on FIELD_DEFINITION
+
+  directive @hasPermission(
+    grant: String,
+  ) on FIELD_DEFINITION
+
+  type Query {
+    read: [String] @hasPermission(grant: "read")
+  }
+
+  type Mutation {
+    publish(txt: String): Int @hasRole(type: "publisher") @hasPermission(grant: "write")
+  }
+`
+
+  const resolvers = {
+    Query: {
+      read: async (_) => ['txt']
+    },
+    Mutation: {
+      publish: async (_, { txt }) => 42
+    }
+  }
+
+  app.register(mercurius, {
+    schema,
+    resolvers
+  })
+
+  app.register(mercuriusAuth, {
+    authContext (context) {
+      return {
+        role: context.reply.request.headers['x-role']
+      }
+    },
+    async applyPolicy (authDirectiveAST, parent, args, context, info) {
+      return context.auth.role === 'publisher'
+    },
+    authDirective: 'hasRole'
+  })
+
+  app.register(mercuriusAuth, {
+    authContext (context) {
+      return {
+        permission: context.reply.request.headers['x-permission']
+      }
+    },
+    async applyPolicy (authDirectiveAST, parent, args, context, info) {
+      return context.auth.permission === authDirectiveAST
+        .arguments.find(arg => arg.name.value === 'grant').value.value
+    },
+    authDirective: 'hasPermission'
+  })
+
+  const query = `query {
+    read
+  }`
+  const mutation = `mutation {
+    publish(txt:"hello")
+  }`
+
+  {
+    const response = await app.inject({
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      url: '/graphql',
+      body: JSON.stringify({ query })
+    })
+
+    t.same(JSON.parse(response.body), {
+      data: {
+        read: null
+      },
+      errors: [
+        {
+          message: 'Failed auth policy check on read',
+          locations: [
+            { line: 2, column: 5 }
+          ],
+          path: ['read']
+        }
+      ]
+    })
+  }
+
+  {
+    const response = await app.inject({
+      method: 'POST',
+      headers: { 'content-type': 'application/json', 'X-permission': 'read' },
+      url: '/graphql',
+      body: JSON.stringify({ query })
+    })
+
+    t.same(JSON.parse(response.body), {
+      data: {
+        read: ['txt']
+      }
+    })
+  }
+
+  {
+    const response = await app.inject({
+      method: 'POST',
+      headers: { 'content-type': 'application/json', 'x-role': 'publisher', 'X-permission': 'read' },
+      url: '/graphql',
+      body: JSON.stringify({ query: mutation })
+    })
+
+    t.same(JSON.parse(response.body), {
+      data: {
+        publish: null
+      },
+      errors: [
+        {
+          message: 'Failed auth policy check on publish',
+          locations: [
+            { line: 2, column: 5 }
+          ],
+          path: ['publish']
+        }
+      ]
+    })
+  }
+
+  {
+    const response = await app.inject({
+      method: 'POST',
+      headers: { 'content-type': 'application/json', 'x-role': 'publisher', 'X-permission': 'write' },
+      url: '/graphql',
+      body: JSON.stringify({ query: mutation })
+    })
+
+    t.same(JSON.parse(response.body), {
+      data: {
+        publish: 42
       }
     })
   }
