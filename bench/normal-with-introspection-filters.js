37b76b551d5f7c4ac3a611dfa51143a901c8b39c
Jonny Green <hello@jonnydgreen.com>
2022-05-25 00:02:38 +0100

    Add benchmarks and remove dependabot todo

---
+++ bench/normal-with-introspection-filters.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mercurius = require('mercurius')
+const mercuriusAuth = require('..')
+const { schema, resolvers } = require('./normal-setup')
+
+const app = Fastify()
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: false,
+  jit: 1
+})
+
+app.register(mercuriusAuth, {
+  authContext (context) {
+    return {
+      identity: context.reply.request.headers['x-user']
+    }
+  },
+  async applyPolicy (authDirectiveAST, parent, args, context, info) {
+    return context.auth.identity === 'admin'
+  },
+  authDirective: 'auth',
+  filterSchema: true
+})
+
+app.listen(3000)
